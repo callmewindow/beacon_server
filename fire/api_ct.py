f9ce7f20483b8bdc5456ff1ded3d2c6efc994107
ctttt1119 <1121447051@qq.com>
2020-09-11 15:52:35 +0800

    update /fire/api_ct.py

--- fire/api_ct.py
+++ fire/api_ct.py
@@ -3,8 +3,10 @@ from fire.models import *
 from django.http.response import HttpResponse
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
+from django.db.models import Max 
 
 
+#获取帖子信息，包括帖子的"标题"、"发帖人"、"发帖时间"、"内容"、"点赞(like)数"，以及该帖子下的所有"回复"。回复包括每条回复的"内容"、"回复人"、"时间"
 def getPostInfo(request):
     if(request.method!='GET'):
         return None
@@ -25,13 +27,75 @@ def getPostInfo(request):
         post_time = str(floor_1_res.post_time)
         post_content = floor_1_res.content
         like_num = str(floor_1_res.like_num)
-        reply = Floor.objects.filter(post_id=post_id).values_list("content","userinfo__username","post_time")
+        topped = str((floor_1_res.topped))
+        watches = str((post_res.watches))
+        stared = str((floor_1_res.stared))
+        reply = Floor.objects.filter(post_id=post_id).values("id","content","userinfo__username","post_time","floor_num")
+        reply_list = []
+        for i in reply:
+            tmp = {'id':i.id, 'content':i.content, 'author':i.userinfo__unsername, 'datetime':str(i.post_time), 'floor':str(i.floor_num)}
+            reply_list.append(tmp)
 
-        content = {'msg':'ok', '标题': post_res.title, '发帖人':post_author, '发帖时间':post_time, '内容':post_content, '点赞数':like_num, '回复':reply}
+        content = {'title': post_res.title, 'author':post_author, 'datetime':post_time, 'content':post_content, 'read':watches, 'like':like_num, 'top':topped, 'highlight':stared, 'reply_list':reply}
         return JsonResponse(content)
+
+
+#回复帖子
+def replyPost(request):
+    if(request.method!='POST'):
+        return None
+    dict = request.GET
+    post_id = dict.get('postId')
+    author_id = dict.get('author')
+    reply_content = dict.get('content')
+    reply_time = dict.get('datetime')
+    floor_num = dict.get('floor')
+    try:
+        new_reply = Floor.objects.create(author_id=author_id, post_id=post_id, content=reply_content, post_time=reply_time, floor_num=floor_num)
+        res = 'ok'
+    except Exception:
+        res = 'api_ct replyPost error'
+    if(res = 'api_ct replyPost error'):
+        err_msg = "api_ct replyPost error."
+        msg2 = "{\"err_msg\": \"" + err_msg + "\"}"
+        return HttpResponse(msg2) 
+    else:
+        msg = "ok"
+        msg2 = "{\"msg\": \"" + msg + "\"}"
+        return HttpResponse(msg2) 
+
+#显示圈子下的所有帖子
+def cicleAllPost(request):
+    if(request.method!='POST'):
+        return None
+    dict = request.GET
+    cicle_id = dict.get('forumId')
+    try:
+        res = Post.objects.filter(course_id=cicle_id)
+    except Exception:
+        post_res = 'api_ct cicleAllPost error'
+    if (post_res=='api_ct cicleAllPost error'):
+        err_msg = "api_ct cicleAllPost error."
+        msg2 = "{\"err_msg\": \"" + err_msg + "\"}"
+        return HttpResponse(msg2) 
+    else:
+        post_list = []
+        for i in res:
+            author_name = Userinfo.objects.filter(id=i.id).username
+            floor_1_res = Floor.objects.filter(Q(post_id=i.id) & Q(floor_num =1))
+            reply_num = Floor.objects.filter(post_id=i.id).aggregate(Max('floor_num')).floor_num__max-1
+            content = {'id':str(i.id), 'title':i.title, 'author':author_name, 'datetime':str(floor_1_res.post_time), 'content':floor_1_res.content, 'read':str(i.watches), 'like':str(floor_1_res.like_num), 'reply_num':reply_num ,'top':str(i.topped) ,'highlight':str(i.stared)}
+            post_list.append(content)
+        #msg = "{\"msg\":\"ok\"" + "\"post_list\"" + "\""+ post_list+ "\"" +"}"\
+        #res_dict = {'msg':'ok', 'post_list':post_list}
+        return JsonResponse(post_list)
+
+
 
 
 url_ct = [
 	url('/getPostInfo',getPostInfo),
+    url('/replyPost',replyPost),
+    url('/cicleAllPost',cicleAllPost),
 	
 	]
