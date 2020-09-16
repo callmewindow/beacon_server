58ad073551058db6a22d4089642592f8b0eac02c
ARealDD <547781459@qq.com>
2020-09-16 19:28:27 +0800

    add CASCADE to foreignkeys, and finish 5 apis

--- fire/api_zym.py
+++ fire/api_zym.py
@@ -116,14 +116,165 @@ def get_userinfo_by_userid(request):
 		else:
 			msg['message'] = '用户不存在。'
 			return JsonResponse(msg, safe=False)
+	except:
+		traceback.print_exc()
+		msg['message'] = 'Unexpected Error'
+		return JsonResponse(msg, safe=False)
 
+def get_applications_by_course_id(request):
+	try:
+		dict = request.GET
+		msg = {}
+		msg['message'] = ''
+		courseid = dict.get('course_id', None)
+		if courseid == None:
+			msg['message'] = '课程id不能为空。'
+			return JsonResponse(msg, safe=False)
 
+		list = CourseApplication.objects.filter(course=courseid)
+		unhandled = []
+		accepted = []
+		rejected = []
+		if list:
+			for i in list:
+				app = model_to_dict(i)
+				type = app.get('result', None)
+				if type:
+					if type == 0:
+						unhandled.append(app)
+					elif type == 1:
+						accepted.append(app)
+					elif type == 2:
+						rejected.append(app)
+					else:
+						msg['message'] = '未知类型的申请结果。'
+						return JsonResponse(msg, safe=False)
+			msg['unhandled_applications'] = unhandled
+			msg['accepted_applications'] = accepted
+			msg['rejected_applications'] = rejected
+			msg['message'] = 'OK'
+			return JsonResponse(msg, safe=False)
+		else:
+			msg['message'] = '课程不存在。'
+			return JsonResponse(msg, safe=False)
+	except:
+		traceback.print_exc()
+		msg['message'] = 'Unexpected Error'
+		return JsonResponse(msg, safe=False)
+
+def accept_application(request):
+	try:
+		dict = request.GET
+		msg = {}
+		msg['message'] = ''
+		id = dict.get('course_application_id', None)
+		if id == None:
+			msg['message'] = '申请id不能为空。'
+			return JsonResponse(msg, safe=False)
+		app = CourseApplication.objects.filter(id=id)
+		if app:
+			app.update(result=1)
+			ap = app.first()
+			new = UserCourse()
+			new.user = ap.user
+			new.course = ap.course
+			new.save()
+			msg['message'] = 'OK'
+			return JsonResponse(msg, safe=False)
+		else:
+			msg['message'] = '申请记录不存在。'
+			return JsonResponse(msg, safe=False)
+	except:
+		traceback.print_exc()
+		msg['message'] = 'Unexpected Error'
+		return JsonResponse(msg, safe=False)
+
+def reject_application(request):
+	try:
+		dict = request.GET
+		msg = {}
+		msg['message'] = ''
+		id = dict.get('course_application_id', None)
+		if id == None:
+			msg['message'] = '申请id不能为空。'
+			return JsonResponse(msg, safe=False)
+		app = CourseApplication.objects.filter(id=id)
+		if app:
+			app.update(result=2)
+			msg['message'] = 'OK'
+			return JsonResponse(msg, safe=False)
+		else:
+			msg['message'] = '申请记录不存在。'
+			return JsonResponse(msg, safe=False)
+	except:
+		traceback.print_exc()
+		msg['message'] = 'Unexpected Error'
+		return JsonResponse(msg, safe=False)
+
+def quit_course(request):
+	try:
+		dict = request.GET
+		msg = {}
+		msg['message'] = ''
+		userid = dict.get('user_id', None)
+		courseid = dict.get('course_id', None)
+		if userid == None:
+			msg['message'] = '用户id不能为空。'
+			return JsonResponse(msg, safe=False)
+		if courseid == None:
+			msg['message'] = '课程id不能为空。'
+			return JsonResponse(msg, safe=False)
+		record = UserCourse.objects.filter(user=userid, course=courseid).first()
+		if record:
+			record_dict = model_to_dict(record)
+			identity = record_dict.get('user_identity', None)
+			msg['record'] = record_dict
+			if identity == 0:
+				record.delete()
+				msg['message'] = 'OK'
+				return JsonResponse(msg, safe=False)
+			elif identity == 1:
+				msg['message'] = '用户属于管理员身份，无法退出课程。'
+				return JsonResponse(msg, safe=False)
+			else:
+				msg['message'] = '未知的用户身份。'
+				return JsonResponse(msg, safe=False)
+		else:
+			msg['message'] = '该用户尚未参加改课程。'
+			return JsonResponse(msg, safe=False)
+	except:
+		traceback.print_exc()
+		msg['message'] = 'Unexpected Error'
+		return JsonResponse(msg, safe=False)
+
+def quit_delete(request):
+	try:
+		dict = request.GET
+		msg = {}
+		msg['message'] = ''
+		courseid = dict.get('course_id', None)
+		if courseid == None:
+			msg['message'] = '课程id不能为空。'
+			return JsonResponse(msg, safe=False)
+		course = Course.objects.filter(id=courseid).first()
+		if course:
+			course.delete()
+			msg['message'] = 'OK'
+			return JsonResponse(msg, safe=False)
+		else:
+			msg['message'] = '待删除的课程不存在。'
+			return JsonResponse(msg, safe=False)
 	except:
 		traceback.print_exc()
 		msg['message'] = 'Unexpected Error'
 		return JsonResponse(msg, safe=False)
 
 url_zym = [
-	url('usercourse',get_course_by_userid),
-	url('userdetail',get_userinfo_by_userid),
+	url('usercourse', get_course_by_userid),
+	url('userdetail', get_userinfo_by_userid),
+	url('class/application/getall', get_applications_by_course_id),
+	url('class/application/accept', accept_application),
+	url('class/application/reject', reject_application),
+	url('class/quit', quit_course),
+	url('class/delete', quit_delete),
 	]
