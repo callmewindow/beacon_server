25f5d0756d41473ab186d9e1c9525dd4c41ef48d
ClementineZ <1415162394@qq.com>
2020-09-17 17:52:38 +0800

    2020-9-17-ztw

--- fire/api_ztw.py
+++ fire/api_ztw.py
@@ -113,11 +113,19 @@ def courseQuery(request):
 		dict = request.POST
 		msg = {}
 		
-		qset = Course.objects.all()
-		courses=[]
-		for q in qset:
-			courses.append(model_to_dict_fixed(q))
-		msg['courses']=courses
+		courses = Course.objects.all()
+		msg_courses=[]
+		for course in courses:
+			course_dict = model_to_dict_fixed(course)
+			
+			teacher = course.teacher_id
+			if not teacher:
+				course_dict['teacher']=None
+			else:
+				course_dict['teacher']=model_to_dict_fixed(teacher)
+			course_dict['student_number']=UserCourse.objects.filter(course=course).count()
+			msg_courses.append(course_dict)
+		msg['courses']=msg_courses
 		return HttpResponse(json_raw(msg))
 		
 	except:
@@ -169,8 +177,6 @@ def createPost(request):
 		floor.post = tag
 		floor.content = dict.get('content',None)
 		now = datetime.datetime.now(tz=gettz('Asia/Beijing'))
-		print(now)
-		print(now.strftime('%Y-%m-%d %H:%M:%S'))
 		floor.post_time = now
 		floor.floor_num = 1
 		floor.reply_floor = 0
@@ -285,10 +291,10 @@ def userCourse(request):
 			msg['result']='课程id不能为空。'
 		users = Userinfo.objects.filter(id=user_id)
 		courses = Course.objects.filter(id=course_id)
-		if not user:
+		if not users:
 			msg['result']='该用户不存在。'
 			return HttpResponse(json_raw(msg))
-		if not course:
+		if not courses:
 			msg['result']='该课程不存在。'
 			return HttpResponse(json_raw(msg))
 		user = users.first()
@@ -300,8 +306,29 @@ def userCourse(request):
 			msg['result']='该用户未选择此课程。'
 			return HttpResponse(json_raw(msg))
 		user_course = user_courses.first()
+		
+		videos = Videos.objects.filter(course=course)
+		
+		play_records = []
+		msg_videos = []
+		msg_user_videos = []
+		for video in videos:
+			msg_videos.append(model_to_dict_fixed(video))
+			user_videos = UserVideo.objects.filter(user=user,video=video)
+			if not user_videos:
+				continue
+			user_video = user_videos.first()
+			msg_user_videos.append(model_to_dict(user_video))
+			user_play_records = UserPlayRecords.objects.filter(user_video=user_video)
+			for user_play_record in user_play_records:
+				play_records.append(model_to_dict_fixed(user_play_record))
+		
+		
 		msg['result']='OK'
 		msg['user_course']=model_to_dict_fixed(user_course)
+		msg['user_videos']=msg_user_videos
+		msg['videos']=msg_videos
+		msg['user_play_records']=play_records
 		return HttpResponse(json_raw(msg))
 		
 	except:
@@ -467,6 +494,30 @@ def unlikeFloor(request):
 		traceback.print_exc()
 		msg['result']='Unexpected Error'
 		return HttpResponse(json_raw(msg))
+def hotCourses(request):
+	try:
+		dict = request.POST
+		msg = {}
+		msg_courses = []
+		
+
+		for course in Course.objects.all():
+			msg_courses.append(model_to_dict_fixed(course))
+		def counterFunc(course_dict):
+			key = UserCourse.objects.filter(course=Course.objects.get(id=course_dict['id'])).count()
+			course_dict['student_number']=key
+			return key
+		msg_courses.sort(key=counterFunc,reverse=True)
+		if len(msg_courses)>10:
+			msg_courses = msg_courses[0:10]
+		msg['courses']=msg_courses
+		msg['result']='OK'
+		return HttpResponse(json_raw(msg))
+		
+	except:
+		traceback.print_exc()
+		msg['result']='Unexpected Error'
+		return HttpResponse(json_raw(msg))
 url_ztw = [
 	url('addUser',addUser),
 	url('setCourseRule',setCourseRule),
@@ -479,5 +530,6 @@ url_ztw = [
 	url('getCourseWatches',getCourseWatches),
 	url('oneWatch',oneWatch),
 	url('^likeFloor$',likeFloor),
-	url('unlikeFloor',unlikeFloor)
+	url('unlikeFloor',unlikeFloor),
+	url('hotCourses',hotCourses)
 	]
